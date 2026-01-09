dc419987208308adfe2dc4e920d934f81c03fa6b
cventuralo <cventuralo@uoc.edu>
2026-01-09 10:44:34 +0100

    Afegeixo educacioMap

--- js/educacioMap.js
+++ js/educacioMap.js
@@ -135,9 +135,9 @@ function drawEducationCounter(svg, width, height, data, year) {
 
   const totalYear = d3.sum(dataYear, d => +d.Valor);
 
-  const boxWidth = 350;
+  const boxWidth = 360;
   const boxHeight = 90;
-  const rightMargin = 70;
+  const rightMargin = 20;
   const bottomMargin = 30;
 
   const xPos = width - boxWidth - rightMargin;
